b1492f6edff1c7fd10dfa309702e25ce6d623b28
irviinvelasco <143757949+irviinvelasco@users.noreply.github.com>
2024-05-16 11:58:40 -0600

    Add files via upload

---
+++ monitor/js/main.js
@@ -0,0 +1,34 @@
+
+let respuesta = document.getElementById("respuesta");
+
+
+function callApiRequest() {
+    axios
+        .get(
+            "http://localhost/iot-car-control/back-end/apis/getRegistro.php")
+        .then(function (response) {
+            // manejar respuesta exitosa
+            console.log(response);
+
+            let respuestaServidor = 
+            response.data == "f" ? "ADELANTE" : 
+            response.data == "b" ? "ATRAS" : 
+            response.data == "l" ? "IZQUIERDA" : 
+            response.data == "r" ? "DERECHA" : 
+            response.data == "s" ? "DETENER" : 
+            response.data;
+            console.log(respuestaServidor);
+
+            respuesta.innerHTML = "Respuesta: <strong>" + respuestaServidor + "</strong>";
+        })
+        .catch(function (error) {
+            // manejar error
+            console.log(error);
+        })
+        .finally(function () {
+            // siempre sera executado
+        });
+}
+setInterval(callApiRequest, 2000);
+
+
