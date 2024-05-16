25d9a1fed6e98de4cddbe8f69a2f720d2f4a74fb
irviinvelasco <143757949+irviinvelasco@users.noreply.github.com>
2024-05-16 11:59:50 -0600

    Update main.js

--- monitor/js/main.js
+++ monitor/js/main.js
@@ -5,7 +5,7 @@ let respuesta = document.getElementById("respuesta");
 function callApiRequest() {
     axios
         .get(
-            "http://localhost/iot-car-control/back-end/apis/getRegistro.php")
+            "https://3.137.212.185/iot-car-control/back-end/apis/getRegistro.php")
         .then(function (response) {
             // manejar respuesta exitosa
             console.log(response);
